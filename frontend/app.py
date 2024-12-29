0960f721435850a5161b7a0129d5081932ae8562
João Vitor Ferreira <jvferreira.20@gmail.com>
2024-12-29 20:02:56 -0300

    Simplificando o arquivo

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
-import dill as pickle
-from dataclasses import dataclass
+import pickle
 import logging
 
 app = Flask(__name__)
@@ -13,29 +12,21 @@ MODEL_PATH = '/data/association_rules.pkl'
 # Configuração do logger
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-@dataclass
-class CustomModel:
-    rules: list
-    model_date: str = None
-
 def load_model(path):
-    print('a')
     try:
         with open(path, 'rb') as file:
-            return pickle.load(file) 
+            return pickle.load(file)  
     except Exception as e:
         logging.error(f"Erro ao carregar o modelo: {e}")
         return None
 
-# Carregando o modelo no início da aplicação
-custom_model = load_model(MODEL_PATH)
+rules_model = load_model(MODEL_PATH)
 
-def get_recommendations(songs, custom_model):
-    if not custom_model or not custom_model.rules:
+def get_recommendations(songs, model):
+    if not model:
         raise ValueError("Modelo não está disponível ou é inválido.")
 
     recommended_playlist = set()
-    model = custom_model.rules
 
     for rule in model:
         rule_song_0 = list(rule[0])[0]
@@ -61,7 +52,7 @@ def recommender():
         if not isinstance(songs, list) or not all(isinstance(song, str) for song in songs):
             raise ValueError("A entrada 'songs' deve ser uma lista de strings.")
 
-        recommended_playlist = get_recommendations(songs, custom_model)
+        recommended_playlist = get_recommendations(songs, rules_model)
 
         response = {
             "playlist": recommended_playlist,
